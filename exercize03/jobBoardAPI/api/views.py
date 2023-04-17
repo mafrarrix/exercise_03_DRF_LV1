a4e6cbcc847c9bfa0e1961d7560d27cf6b4314fe
mafrarrix <devapp.vs@outlook.com>
2023-04-17 22:45:31 +0200

    add method APIview

--- exercize03/jobBoardAPI/api/views.py
+++ exercize03/jobBoardAPI/api/views.py
@@ -19,20 +19,30 @@ class JobOfferListeCreateAPIview(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors), status=status.HTTP_400_BAD_REQUEST
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
- class JobOfferDetailAPIview(APIView):
+class JobOfferDetailAPIview(APIView):
      
-     def get_object(self, pk)
-        JobOffer = get_object_or_404(JobOffer, pk=pk)
+    def get_object(self, pk):
+        JobOffer = self.get_object_or_404(JobOffer, pk=pk)
         return JobOffer
     
-     def get(self, request, pk):
-          JobOffer = get_object(pk)
-          serilizer = JobOfferSerializer(JobOffer)
-          return Response(serilizer.data)
+    def get(self, request, pk):
+        JobOffer = self.get_object(pk)
+        serilizer = JobOfferSerializer(JobOffer)
+        return Response(serilizer.data)
     
-     #def put(self, request, pk):
-        
+    def put(self, request, pk):
+        JobOffer = self.get_object(pk)
+        erializer = JobOfferSerializer(JobOffer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        JobOffer = self.get_object(pk)
+        JobOffer.delete
+        return Response(serializer.errors, status=status.HTTP_204_NO_CONTENT)
      
     
