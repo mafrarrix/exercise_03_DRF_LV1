afa123db4ba376ad7402b9feabd288614fe96e7a
mafrarrix <devapp.vs@outlook.com>
2023-05-26 19:43:57 +0200

    refactor

--- exercise03/exercise03/urls.py
+++ exercise03/exercise03/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for exercize03 project.
+URL configuration for exercise03 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- exercise03/exercise03/wsgi.py
+++ exercise03/exercise03/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exercize03 project.
+WSGI config for exercise03 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exercize03.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exercise03.settings')
 
 application = get_wsgi_application()
